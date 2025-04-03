f0f5d3b37b5c052e67a175c8294d2c9a331e472b
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-04-03 22:27:52 +0530

    update border

--- src/components/todo-header.tsx
+++ src/components/todo-header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
